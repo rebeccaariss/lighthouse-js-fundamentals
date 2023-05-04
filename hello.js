e32421a14561c75bfb88c57b0c97c7928c2df501
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-05-04 16:46:21 -0400

    Practice with console.log vs. return

--- hello.js
+++ hello.js
@@ -7,10 +7,28 @@
 
 
 // <<< Example 2: >>>
-const sayHello  = function (name) {
-  console.log("Hello, " + name);
-}
+// const sayHello  = function (name) {
+//   console.log("Hello, " + name);
+// }
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+//  <<< Example 3: >>>
+//  <<< console.log vs. return: >>>
+// const sayHelloToConsole  = function (name) {
+//   console.log("Hello, " + name);
+// }
+// sayHelloToConsole('Jamie'); 
+
+// const returnSayHello  = function (name) {
+//   return "Hello, " + name;
+// }
+// const greeting = returnSayHello('Jamie');
+
+// const returnSayHello  = function (name) {
+//   return "Hello, " + name;
+// }
+// const greeting = returnSayHello('Jamie');
+// console.log(greeting);
