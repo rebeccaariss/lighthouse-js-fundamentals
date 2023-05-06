dc1b6ab3f7f82f2d77343bb162793710acdc9092
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-05-06 13:37:12 -0400

    Same outcome but with a while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
+// <<< Example 1: Iterate over items in an array with a for loop: >>>
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// <<< Example 2: Iterate over items in an array with a while loop: >>>
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
