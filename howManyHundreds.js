ac612f2c0eedebcfe6066424810a1d5f896e5b8c
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-05-06 07:10:53 -0400

    Initial commit

---
+++ howManyHundreds.js
@@ -0,0 +1,10 @@
+const howManyHundreds = function(num) {
+  const evenNum = num - (num % 100);
+  return evenNum / 100;
+};
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
